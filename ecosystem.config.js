a4ab4cc9f2e7ab6b71c656f99c4e79ba2d3d4ca0
SkuuIll <moreappmix@gmail.com>
2026-05-04 19:42:11 -0300

    feat: add PM2 ecosystem configuration for production deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: 'showroom-jr',
+      script: 'npm',
+      args: 'start',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '5G',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000
+      }
+    }
+  ]
+}
